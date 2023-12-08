60e4a0ee42747029fad6714c83d56f6864d1415b
Robert Schroll <robert@thedataincubator.com>
2023-12-08 02:24:50 +0000

    Filter database results by min depth and gradient
    We were careful to use a parameterized query to avoid SQL injection.

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
+import sys
 import sqlalchemy
+
+mindepth = sys.argv[1]
+mingradient = sys.argv[2]
 
 conn_string = "postgresql://tdi:qKmd8s5ze7WAYV@adventureworks.tditrain.com:5431/wells"
 engine = sqlalchemy.create_engine(conn_string)
 conn = engine.connect()
-result = conn.execute("""
-    SELECT latitude, longitude, depth, gradient FROM wells LIMIT 10;
+query = sqlalchemy.text("""
+    SELECT latitude, longitude, depth, gradient FROM wells 
+    WHERE depth > :mindepth AND gradient > :mingradient;
 """)
+result = conn.execute(query, {'mindepth': mindepth, 'mingradient': mingradient})
 rows = result.fetchall()
 print(rows)
