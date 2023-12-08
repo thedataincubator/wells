918cdb1765f16c417cc48567ca126f303492ffcd
Robert Schroll <robert@thedataincubator.com>
2023-12-08 02:33:39 +0000

    Make database.py into a proper module

--- database.py
+++ database.py
@@ -1,16 +1,19 @@
 import sys
 import sqlalchemy
 
-mindepth = sys.argv[1]
-mingradient = sys.argv[2]
+_conn_string = "postgresql://tdi:qKmd8s5ze7WAYV@adventureworks.tditrain.com:5431/wells"
 
-conn_string = "postgresql://tdi:qKmd8s5ze7WAYV@adventureworks.tditrain.com:5431/wells"
-engine = sqlalchemy.create_engine(conn_string)
-conn = engine.connect()
-query = sqlalchemy.text("""
-    SELECT latitude, longitude, depth, gradient FROM wells 
-    WHERE depth > :mindepth AND gradient > :mingradient;
-""")
-result = conn.execute(query, {'mindepth': mindepth, 'mingradient': mingradient})
-rows = result.fetchall()
-print(rows)
+def query_db(mindepth, mingradient):
+    engine = sqlalchemy.create_engine(_conn_string)
+    conn = engine.connect()
+    query = sqlalchemy.text("""
+        SELECT latitude, longitude, depth, gradient FROM wells 
+        WHERE depth > :mindepth AND gradient > :mingradient;
+    """)
+    result = conn.execute(query, {'mindepth': mindepth, 'mingradient': mingradient})
+    return result.fetchall()
+
+if __name__ == '__main__':
+    mindepth = sys.argv[1]
+    mingradient = sys.argv[2]
+    print(query_db(mindepth, mingradient))
