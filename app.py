b79d1436b8cab6b6e8e352b05aee7ff96bde1788
Robert Schroll <robert@thedataincubator.com>
2024-01-17 02:28:59 +0000

    Let user specify min depth and gradient

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ from plotting import plot_wells
 
 def app():
     st.title('Wells in the US')
+    st.markdown('Select the minimum depth and gradient to display:')
     
-    data = query_db(5000, 0.01)
+    min_depth = st.number_input('Min depth', 0, 10000, step=500, value=5000)
+    min_gradient = st.number_input('Min gradient', 0., 0.1, step=0.005, value=0.01, format='%0.3f')
+    
+    data = query_db(min_depth, min_gradient)
     st.write(plot_wells(data))
-    
     
 if __name__ == '__main__':
     app()
